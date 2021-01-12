4460c9e9c93adeb2c22aeaa7c9c95f1fd9d0899a
Artem Kondratyev <walinor.88@gmail.com>
2021-01-12 15:35:44 +0300

    dev: Добавление проекта Calorie-Zen, на React

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import './AppContainer.css';
+import AppContainer from './AppContainer.js';
+import Header from './Header';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <AppContainer />
+    </>
+  );
+}
+
+export default App;
