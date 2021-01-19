0c5f8a4396af1873e0367dec1231e663f0b05469
Artem Kondratyev <walinor.88@gmail.com>
2021-01-19 16:22:39 +0300

    Маршрут tips

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,19 @@ import React from 'react';
 import './App.css';
 import Header from './Header';
 import Diary from './Diary';
-import { BrowserRouter } from "react-router-dom";
+import Tips from "./Tips";
+import { BrowserRouter, Route } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
-      <Diary />
+      <Route path="/">
+        <Diary />
+      </Route>
+      <Route path="/tips">
+        <Tips />
+      </Route>
     </BrowserRouter>
   );
 }
