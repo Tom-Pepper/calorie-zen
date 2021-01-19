a4104eb1c2d14f060a7810efcf89bd06062f9fd1
Artem Kondratyev <walinor.88@gmail.com>
2021-01-19 17:56:37 +0300

    dev: добавлены ссылки NavLink к страничкам

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './NavBar.css';
-import { NavLink } from 'react-router-dom';
+import { NavLink } from "react-router-dom";
 
 function NavBar() {
   return (
     <nav className="menu">
-      <NavLink className="menu__item" activeClassName="menu__item_active" exact to="/">Home</NavLink>
-      <NavLink className="menu__item" activeClassName="menu__item_active" to="/tips">Tips</NavLink>
+      <NavLink exact to="/" className="menu__item" activeClassName="menu__item_active">Домой</NavLink>
+      <NavLink to="/tips" className="menu__item" activeClassName="menu__item_active">Советы</NavLink>
     </nav>
   );
 }
